b3f4dd4a0ffa60869a1edb6937258a61c956f238
danaisboss <yjk9313@gmail.com>
2021-10-29 00:01:40 +0900

    ENTITY, REPOSITORY 추가

--- README.md
+++ README.md
@@ -16,3 +16,14 @@
 - data transformation, data validation을 위해 사용
   - 받은 데이터를 올바른 형식으로 변환 ex : `1234 -> '1234'`
   - 데이터의 유효성 검사를 하기위해 사용 (데이터의 유무 등..)
+
+`entity` :
+
+- entity란 데이터베이스 테이블에 대한 클래스이다.
+- 엔티티 클래스를 작성하면 typeORM은 해당 엔티티를 가지고 데이터테이블을 만든다.
+
+`Repository Pattern` :
+
+- Repository는 entity object와 함께 동작한다. entity들을 find, insert, update, delete 한다.
+- 데이터에 관련된 동작은 service단에서 하지 않고 repository에서 한다.
+- 이를 repository pattern이라고 하기도 한다.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './boards/board.repository';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    BoardsModule,
+    TypeOrmModule.forRoot(typeORMConfig),
+    TypeOrmModule.forFeature([BoardRepository]),
+  ],
 })
 export class AppModule {}
